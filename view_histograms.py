acacee69bf0d35065495304a609188995f7eac0c
Xiaoxuan <146991491+XiaoxuanLi35@users.noreply.github.com>
2025-01-03 16:02:17 +0000

    Update view_histograms.py

--- view_histograms.py
+++ view_histograms.py
@@ -2,7 +2,7 @@ import numpy as np
 from pathlib import Path
 
 # Specify color_histogram folder path
-histogram_path = Path(r"C:\Users\李晓璇\Desktop\Semester 3\CS6123\project_3\scikit-image\Forage\color_histogram")
+histogram_path = Path(r"C:\Users\name\Desktop\Semester 3\CS6123\project_3\scikit-image\Forage\color_histogram")
 
 # Read and view all histograms
 for npy_file in histogram_path.glob('*.npy'):
